892a4caab7d0359c47f56e8d921e03d30fa25e13
MatteoCurti99 <92578592+MatteoCurti99@users.noreply.github.com>
2023-05-04 12:01:45 +0100

    Project C++ Empty
    Added C++ Project

---
+++ MagnetMechanicsTP/Intermediate/Build/Win64/MagnetMechanicsTPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30148
+#include "K:/UnrealProject/MagnetMechanicsTP/Intermediate/Build/Win64/MagnetMechanicsTPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ MagnetMechanicsTP/Source/MagnetMechanicsTP/MagnetMechanicsTPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MagnetMechanicsTPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGNETMECHANICSTP_API AMagnetMechanicsTPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
